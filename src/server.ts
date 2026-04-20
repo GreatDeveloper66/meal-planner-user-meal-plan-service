264f75fb6a653ba448694bfebb7c50c6a22c7844
GreatDeveloper66 <ashaffe@gmail.com>
2026-04-19 21:37:10 -0500

    add connectDB to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import { connectDB } from './config/db';
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+
+connectDB();
+
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
