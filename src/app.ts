c26bb02ce3a8ed95c809845ec6d402fe7d2c384b
GreatDeveloper66 <ashaffe@gmail.com>
2026-04-24 11:17:46 -0500

    Add create user plan post routes to app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,20 @@
 import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
+import { createUserMealPlan, createUserMealPlanWithPrice } from './controllers/Controllers.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+await connectDB(); // 👈 connect on demand
+
+app.post('/createUserMealPlan', createUserMealPlan);
+app.post('/createUserMealPlanWithPrice', createUserMealPlanWithPrice);
+
+
 app.get('/', async (req, res) => {
-  await connectDB(); // 👈 connect on demand
   res.send('Hello, World!');
 });
 
