6e6460d6339ef23109179de204ca18fdd143fcd5
GreatDeveloper66 <ashaffe@gmail.com>
2026-04-29 12:47:07 -0500

    update imports and routes for app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,21 @@
 import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
-import { createUserMealPlan, createUserMealPlanWithPrice } from './controllers/Controllers.js';
+import { createUserMealPlan, getUserMealPlan, updateUserMealPlan, deleteUserMealPlan} from './controllers/Controllers.js';
+import { authenticationMiddleware } from './middleware/Middleware.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(authenticationMiddleware);
 
 await connectDB(); // 👈 connect on demand
 
 app.post('/createUserMealPlan', createUserMealPlan);
-app.post('/createUserMealPlanWithPrice', createUserMealPlanWithPrice);
+app.get('/getUserMealPlan', getUserMealPlan);
+app.patch('/updateUserMealPlan', updateUserMealPlan);
+app.delete('/deleteUserMealPlan', deleteUserMealPlan);
 
 
 app.get('/', async (req, res) => {
